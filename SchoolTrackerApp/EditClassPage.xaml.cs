05fb71d3ba5569c3220e4a3b43a11b6b92e8ad9c
spilgr3 <spilgr3@wgu.edu>
2026-08-18 23:10:03 -0700

    Snapshot implemented where the only way to save is through the save button

--- SchoolTrackerApp/EditClassPage.xaml.cs
+++ SchoolTrackerApp/EditClassPage.xaml.cs
@@ -4,9 +4,35 @@ namespace SchoolTrackerApp;
 
 public partial class EditClassPage : ContentPage
 {
+
+    private readonly string cancelCredits;
+    private readonly string cancelClassID;
+    private readonly string cancelClassName;
+    private readonly DateTime? cancelStartDate;
+    private readonly DateTime? cancelEndDate;
+    private readonly string cancelTeacher;
+    private readonly string cancelTeacherPhone;
+    private readonly string cancelTeacherEmail;
+
     public async void ExitButton_Clicked(object sender, EventArgs e)
     {
+        CancelEdit();
         await Navigation.PopAsync();
+    }
+
+    private void CancelEdit()
+    {
+        if (BindingContext is Classes selectedClass)
+        {
+            selectedClass.Credits = cancelCredits;
+            selectedClass.ClassID = cancelClassID;
+            selectedClass.ClassID = cancelClassName;
+            selectedClass.StartDate  = cancelStartDate;
+            selectedClass.EndDate  = cancelEndDate;
+            selectedClass.Teacher  = cancelTeacher;
+            selectedClass.TeacherPhone  = cancelTeacherPhone;
+            selectedClass.TeacherEmail  = cancelTeacherEmail;
+        }
     }
 
     private async void SaveButtonClicked(object sender, EventArgs e)
@@ -17,5 +43,14 @@ public partial class EditClassPage : ContentPage
     {
         InitializeComponent();
         BindingContext = selectedClass;
+
+        cancelCredits = selectedClass.Credits;
+        cancelClassID = selectedClass.ClassID;
+        cancelClassName = selectedClass.ClassName;
+        cancelStartDate = selectedClass.StartDate;
+        cancelEndDate = selectedClass.EndDate;
+        cancelTeacher = selectedClass.Teacher;
+        cancelTeacherPhone = selectedClass.TeacherPhone;
+        cancelTeacherEmail = selectedClass.TeacherEmail;
     }
 }
--- SchoolTrackerApp/obj/Debug/net10.0-ios/iossimulator-arm64/SchoolTrackerApp.AssemblyInfo.cs
+++ SchoolTrackerApp/obj/Debug/net10.0-ios/iossimulator-arm64/SchoolTrackerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+05ffac9209a980a417ccc657795c1d1a691e8f88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3d1160f847239e58955ed05dbc530798a7e0d85f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
